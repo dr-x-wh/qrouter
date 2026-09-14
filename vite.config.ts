82c7a0b7156613ee3a60d2d81b6e576d89d27bc2
drx <dr.xiong.wen.hao@gmail.com>
2026-09-14 14:17:13 +0800

    version: v0.0.1

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(
+                import.meta.dirname,
+                "src/index.ts",
+            ),
+
+            formats: ["es"],
+
+            fileName: () => "index.js",
+        },
+
+        rolldownOptions: {
+            external: [
+                "vue",
+                "vue-router",
+            ],
+        },
+
+        sourcemap: true,
+        minify: false,
+    },
+});
